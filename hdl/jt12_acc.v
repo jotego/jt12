a558597021d2a164c291f8e0de2b1ea72936b318
Jose Tejada <jotego@gmail.com>
2018-11-03 09:32:05 +0100

    minor improvement to x2 volume gain

--- hdl/jt12_acc.v
+++ hdl/jt12_acc.v
@@ -130,11 +130,12 @@ always @(posedge clk)
 		end
         if( s2_enters ) begin
         	sum_all <= 1'b0;
+        	// x1 volume
 			// left  <= pre_left;
 			// right <= pre_right;
 			// x2 volume
-			left <= { pre_left[10:0], 1'b0 };
-			right <= { pre_right[10:0], 1'b0 };
+			left  <= { pre_left [10:0], pre_left [10] };
+			right <= { pre_right[10:0], pre_right[10] };
             `ifdef DUMPSOUND
             $strobe("%d\t%d", right, right);
             `endif
