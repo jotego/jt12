8df6992ff894a00ed66fa6b9561099d9d1cd9990
greyrogue <greyrogue@gmail.com>
2019-11-11 21:42:52 -0500

    Add missing data passthrough
    The data lines were accidentally left out of this file.

--- hdl/jt10.v
+++ hdl/jt10.v
@@ -40,6 +40,7 @@ module jt10(
     input   [7:0]   adpcma_data,  // Data from RAM
     output  [23:0]  adpcmb_addr,  // real hardware has 12 pins multiplexed through PMPX pin
     output          adpcmb_roe_n, // ADPCM-B ROM output enable
+    input   [7:0]   adpcmb_data,
     // Separated output
     output          [ 7:0] psg_A,
     output          [ 7:0] psg_B,
@@ -73,6 +74,7 @@ u_jt12(
     .adpcma_data    ( adpcma_data  ), // Data from RAM
     .adpcmb_addr    ( adpcmb_addr  ), // real hardware has 12 pins multiplexed through PMPX pin
     .adpcmb_roe_n   ( adpcmb_roe_n ), // ADPCM-B ROM output enable
+    .adpcmb_data    ( adpcmb_data  ), // Data from RAM
     // Separated output
     .psg_A          ( psg_A        ),
     .psg_B          ( psg_B        ),
