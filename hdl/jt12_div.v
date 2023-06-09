jotego/jt12

commit d4607e1ec4857c7aab4660162e2da53d02fa3890

Makes it more probable that the synthesizer uses the clock enable inputs

hdl/jt12_div.v
@@ -25,12 +25,12 @@ module jt12_div(
     input           clk,
     input           cen /* synthesis direct_enable */,
     input   [1:0]   div_setting,
-    output  reg     clk_en,      // after prescaler
-    output  reg     clk_en_2,    // cen divided by 2
-    output  reg     clk_en_ssg,
-    output  reg     clk_en_666,  // 666 kHz
-    output  reg     clk_en_111,  // 111 kHz
-    output  reg     clk_en_55    //  55 kHz
+    (* direct_enable *) output  reg     clk_en,      // after prescaler
+    (* direct_enable *) output  reg     clk_en_2,    // cen divided by 2
+    (* direct_enable *) output  reg     clk_en_ssg,
+    (* direct_enable *) output  reg     clk_en_666,  // 666 kHz
+    (* direct_enable *) output  reg     clk_en_111,  // 111 kHz
+    (* direct_enable *) output  reg     clk_en_55    //  55 kHz
 );
 
 parameter use_ssg=0;
@@ -81,6 +81,17 @@ initial clk_en_666 = 1'b0;
 reg cen_55_int;
 reg [1:0] div2=2'b0;
 
+reg pre_clk_en, pre_clk_en_2, pre_clk_en_ssg, pre_clk_en_666, pre_clk_en_111, pre_clk_en_55;
+
+always @(negedge clk) begin     // It's important to leave the negedge to use the physical clock enable input
+    clk_en     <= pre_clk_en;
+    clk_en_2   <= pre_clk_en_2;
+    clk_en_ssg <= pre_clk_en_ssg;
+    clk_en_666 <= pre_clk_en_666;
+    clk_en_111 <= pre_clk_en_111;
+    clk_en_55  <= pre_clk_en_55;
+end
+
 always @(posedge clk) begin
     cen_int        <= opn_cnt      == 4'd0;
     cen_ssg_int    <= ssg_cnt      == 3'd0;
@@ -88,18 +99,18 @@ always @(posedge clk) begin
     cen_adpcm3_int <= adpcm_cnt111 == 3'd0;
     cen_55_int     <= adpcm_cnt55  == 3'd0;
     `ifdef FASTDIV
-    // always enabled for fast sims (use with GYM output, timer will not work well)
+    // always enabled for fast sims (use with GYM output, the timers will not work well)
     clk_en     <= 1'b1;
     clk_en_ssg <= 1'b1;
     clk_en_666 <= 1'b1;
     clk_en_55  <= 1'b1;
     `else
-    clk_en     <= cen & cen_int;   
-    clk_en_2   <= cen && (div2==2'b00);
-    clk_en_ssg <= use_ssg ? (cen & cen_ssg_int) : 1'b0;
-    clk_en_666 <= cen & cen_adpcm_int; 
-    clk_en_111 <= cen & cen_adpcm_int & cen_adpcm3_int; 
-    clk_en_55  <= cen & cen_adpcm_int & cen_adpcm3_int & cen_55_int;
+    pre_clk_en     <= cen & cen_int;
+    pre_clk_en_2   <= cen && (div2==2'b00);
+    pre_clk_en_ssg <= use_ssg ? (cen & cen_ssg_int) : 1'b0;
+    pre_clk_en_666 <= cen & cen_adpcm_int;
+    pre_clk_en_111 <= cen & cen_adpcm_int & cen_adpcm3_int;
+    pre_clk_en_55  <= cen & cen_adpcm_int & cen_adpcm3_int & cen_55_int;
     `endif
 end
 

hdl/jt12_top.v
@@ -27,7 +27,7 @@ http://gendev.spritesmind.net/forum/viewtopic.php?t=386&postdays=0&postorder=asc
 module jt12_top (
     input           rst,        // rst should be at least 6 clk&cen cycles long
     input           clk,        // CPU clock
-    input           cen,        // optional clock enable, it not needed leave as 1'b1
+    (* direct_enable *) input cen,        // optional clock enable, if not needed leave as 1'b1
     input   [7:0]   din,
     input   [1:0]   addr,
     input           cs_n,

commit 8d0c2f7abfd51eabbabf74593d5888c4bea2a41f

Fix cen generation for ADPCM-A
Commit d4607e1ec4857c7aab4660162e2da53d02fa3890 broke ADPCM-A

This one fixes it for NeoGeo.

hdl/jt12_div.v
@@ -86,8 +86,8 @@ always @(negedge clk) begin     // It's important to leave the negedge to use th
     clk_en     <= pre_clk_en;
     clk_en_2   <= pre_clk_en_2;
     clk_en_ssg <= pre_clk_en_ssg;
-    clk_en_666 <= pre_clk_en_666;
-    clk_en_111 <= pre_clk_en_111;
+    clk_en_666 <= cen && pre_clk_en_666;
+    clk_en_111 <= cen && pre_clk_en_111;
     clk_en_55  <= pre_clk_en_55;
 end
 
@@ -107,8 +107,8 @@ always @(posedge clk) begin
     pre_clk_en     <= cen & cen_int;
     pre_clk_en_2   <= cen && (div2==2'b00);
     pre_clk_en_ssg <= use_ssg ? (cen & cen_ssg_int) : 1'b0;
-    pre_clk_en_666 <= cen & cen_adpcm_int;
-    pre_clk_en_111 <= cen & cen_adpcm_int & cen_adpcm3_int;
+    pre_clk_en_666 <= cen_adpcm_int;
+    pre_clk_en_111 <= cen_adpcm_int & cen_adpcm3_int;
     pre_clk_en_55  <= cen & cen_adpcm_int & cen_adpcm3_int & cen_55_int;
     `endif
 end