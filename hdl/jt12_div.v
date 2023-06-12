ef5600dad758902c8b89a91cb085d608a29b1be9
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2023-06-12 14:33:14 +0200

    Revert d4607e1ec4857c7aab4660162e2da53d02fa3890
    The current version of jt12_div causes missing sounds in various
    Neo-Geo games.

--- hdl/jt12_div.v
+++ hdl/jt12_div.v
@@ -80,18 +80,7 @@ initial clk_en_666 = 1'b0;
 reg cen_55_int;
 reg [1:0] div2=2'b0;
 
-reg pre_clk_en, pre_clk_en_2, pre_clk_en_ssg, pre_clk_en_666, pre_clk_en_111, pre_clk_en_55;
-
-always @(negedge clk) begin     // It's important to leave the negedge to use the physical clock enable input
-    clk_en     <= pre_clk_en;
-    clk_en_2   <= pre_clk_en_2;
-    clk_en_ssg <= pre_clk_en_ssg;
-    clk_en_666 <= cen && pre_clk_en_666;
-    clk_en_111 <= cen && pre_clk_en_111;
-    clk_en_55  <= pre_clk_en_55;
-end
-
-always @(posedge clk) begin
+always @(negedge clk) begin
     cen_int        <= opn_cnt      == 4'd0;
     cen_ssg_int    <= ssg_cnt      == 3'd0;
     cen_adpcm_int  <= adpcm_cnt666 == 5'd0;
@@ -100,16 +89,17 @@ always @(posedge clk) begin
     `ifdef FASTDIV
     // always enabled for fast sims (use with GYM output, the timers will not work well)
     clk_en     <= 1'b1;
+    clk_en_2   <= 1'b1;
     clk_en_ssg <= 1'b1;
     clk_en_666 <= 1'b1;
     clk_en_55  <= 1'b1;
     `else
-    pre_clk_en     <= cen & cen_int;
-    pre_clk_en_2   <= cen && (div2==2'b00);
-    pre_clk_en_ssg <= use_ssg ? (cen & cen_ssg_int) : 1'b0;
-    pre_clk_en_666 <= cen_adpcm_int;
-    pre_clk_en_111 <= cen_adpcm_int & cen_adpcm3_int;
-    pre_clk_en_55  <= cen & cen_adpcm_int & cen_adpcm3_int & cen_55_int;
+    clk_en     <= cen & cen_int;
+    clk_en_2   <= cen && (div2==2'b00);
+    clk_en_ssg <= use_ssg ? (cen & cen_ssg_int) : 1'b0;
+    clk_en_666 <= cen & cen_adpcm_int;
+    clk_en_111 <= cen & cen_adpcm_int & cen_adpcm3_int;
+    clk_en_55  <= cen & cen_adpcm_int & cen_adpcm3_int & cen_55_int;
     `endif
 end
 
