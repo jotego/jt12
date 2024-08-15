ba3c29183a5e9ceae7c6f717f4c15b7d2143bc5b
Jose Tejada <jose.tejada@jotego.es>
2024-08-15 09:11:35 +0200

    shift registers synthesized as LUT. Reduces logic cell count by 14% in jt03

--- hdl/jt12_sh_rst.v
+++ hdl/jt12_sh_rst.v
@@ -32,23 +32,12 @@ module jt12_sh_rst #(parameter width=5, stages=32, rstval=1'b0 )
 reg [stages-1:0] bits[width-1:0];
 
 genvar i;
-integer k;
-generate
-initial
-	for (k=0; k < width; k=k+1) begin
-		bits[k] = { stages{rstval}};
-	end
-endgenerate
-
 generate
 	for (i=0; i < width; i=i+1) begin: bit_shifter
-		always @(posedge clk, posedge rst) 
-			if( rst ) begin
-				bits[i] <= {stages{rstval}};
-			end else if(clk_en) begin
-				bits[i] <= {bits[i][stages-2:0], din[i]};
-			end
-		assign drop[i] = bits[i][stages-1];
+		always @(posedge clk) if(clk_en) begin
+			bits[i] <= {bits[i][stages-2:0], din[i]};
+		end
+		assign drop[i] = rst ? {stages{rstval[0]}} : bits[i][stages-1];
 	end
 endgenerate
 
