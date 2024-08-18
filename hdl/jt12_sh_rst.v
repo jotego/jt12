7bbbdcbd0a423595fab9be37ec56524ea3bf3afa
Jose Tejada <jose.tejada@jotego.es>
2024-08-18 08:54:05 +0200

    a bit nicer for timing

--- hdl/jt12_sh_rst.v
+++ hdl/jt12_sh_rst.v
@@ -30,14 +30,15 @@ module jt12_sh_rst #(parameter width=5, stages=32, rstval=1'b0 )
 );
 
 reg [stages-1:0] bits[width-1:0];
+wire [width-1:0] din_mx = rst ? {width{rstval[0]}} : din;
 
 genvar i;
 generate
 	for (i=0; i < width; i=i+1) begin: bit_shifter
 		always @(posedge clk) if(clk_en) begin
-			bits[i] <= {bits[i][stages-2:0], din[i]};
+			bits[i] <= {bits[i][stages-2:0], din_mx[i]};
 		end
-		assign drop[i] = rst ? rstval[0] : bits[i][stages-1];
+		assign drop[i] = bits[i][stages-1];
 	end
 endgenerate
 
